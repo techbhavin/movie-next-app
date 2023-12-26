20cbee8d6064651d53be7242d711be0c81a85d51
bhavin <techbhavin@gmail.com>
2023-12-26 14:43:36 +0530

    add env file and updated readme

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBquLXrd_FMLRQNInXjc2RPruiyudcFTxY",
-  authDomain: "movie-test-29a0f.firebaseapp.com",
-  projectId: "movie-test-29a0f",
-  storageBucket: "movie-test-29a0f.appspot.com",
-  messagingSenderId: "195435343954",
-  appId: "1:195435343954:web:271a4b921e025b91b16f8b",
+  apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_REACT_APP_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
